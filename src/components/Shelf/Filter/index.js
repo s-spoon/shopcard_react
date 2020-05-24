bd9cf748c2c3ae736be029e359fd270b95d060a6
Cleaninja <pswchttopwdm@gmail.com>
2020-05-24 00:22:54 -0300

    Fix Component is defined but never used  no-unused-vars

--- src/components/Shelf/Filter/index.js
+++ src/components/Shelf/Filter/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
@@ -10,47 +10,43 @@ import './style.scss';
 
 const availableSizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL'];
 
-class Filter extends Component {
-  static propTypes = {
-    updateFilters: PropTypes.func.isRequired,
-    filters: PropTypes.array
-  };
+const Filter = props => {
+  const selectedCheckboxes = new Set();
 
-  componentDidMount() {
-    this.selectedCheckboxes = new Set();
-  }
-
-  toggleCheckbox = label => {
-    if (this.selectedCheckboxes.has(label)) {
-      this.selectedCheckboxes.delete(label);
+  const toggleCheckbox = label => {
+    if (selectedCheckboxes.has(label)) {
+      selectedCheckboxes.delete(label);
     } else {
-      this.selectedCheckboxes.add(label);
+      selectedCheckboxes.add(label);
     }
 
-    this.props.updateFilters(Array.from(this.selectedCheckboxes));
+    props.updateFilters(Array.from(selectedCheckboxes));
   };
 
-  createCheckbox = label => (
+  const createCheckbox = label => (
     <Checkbox
       classes="filters-available-size"
       label={label}
-      handleCheckboxChange={this.toggleCheckbox}
+      handleCheckboxChange={toggleCheckbox}
       key={label}
     />
   );
 
-  createCheckboxes = () => availableSizes.map(this.createCheckbox);
+  const createCheckboxes = () => availableSizes.map(createCheckbox);
 
-  render() {
-    return (
-      <div className="filters">
-        <h4 className="title">Sizes:</h4>
-        {this.createCheckboxes()}
-        <GithubStarButton />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="filters">
+      <h4 className="title">Sizes:</h4>
+      {createCheckboxes()}
+      <GithubStarButton />
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  updateFilters: PropTypes.func.isRequired,
+  filters: PropTypes.array
+};
 
 export default connect(
   null,
